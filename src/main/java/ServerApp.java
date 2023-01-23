64c860f5720beffef686d4ddfb86245176acd600
glebov <constantine.glebov@gmail.com>
2023-01-23 15:49:14 +0700

    code fix

--- src/main/java/ServerApp.java
+++ src/main/java/ServerApp.java
@@ -6,26 +6,24 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ServerApp {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
+        System.out.println("Server started...");
         int port = 8085;
 
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
-
-            System.out.println("Server started...");
-
-            while (true) {
-                try (Socket clientSocket = serverSocket.accept();
-                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                ) {
-                    System.out.printf("New connection accepted to port: %d", clientSocket.getPort());
-                    final String name = in.readLine();
-                    out.printf("Hi %s, your port is %d", name, clientSocket.getPort());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+        while (true) {
+            try (ServerSocket serverSocket = new ServerSocket(port);
+                 Socket clientSocket = serverSocket.accept();
+                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+            ) {
+                System.out.printf("New connection accepted to port: %d", clientSocket.getPort());
+                final String name = in.readLine();
+                out.printf("Hi %s, your port is %d", name, clientSocket.getPort());
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
+
     }
 }
